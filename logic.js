765eac5309b7c550a866fe9af76902fe8d02ed9e
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-10-10 16:07:53 +0100

    Complete vue overhaul

--- logic.js
+++
@@ -1,9 +0,0 @@
-const themeBtn = document.querySelector(".btn-toggle");
-themeBtn.addEventListener("click", () => {
-    const body = document.querySelector("body");
-    body.classList.toggle("theme-light");
-    body.classList.toggle("theme-dark");
-    const moonIcon = document.querySelector(".fa-moon");
-    moonIcon.classList.toggle("fa-regular");
-    moonIcon.classList.toggle("fa-solid");
-})
